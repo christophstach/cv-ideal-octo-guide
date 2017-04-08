138e62824ffbc64ba789fb0b7ea0612a82f14207
Christoph Stach <christoph.stach@gmail.com>
2017-04-09 01:27:28 +0200

    WIP

---
+++ src/store.js
@@ -0,0 +1,54 @@
+
+const workingExperience = {
+  title: 'Working experience',
+  employers: [
+    {
+      name: 'SLH GmbH',
+      location: '48465 Schüttorf',
+      dateStart: new Date(),
+      dateEnd: null,
+      projects: [
+
+      ],
+      mainTasks: [
+        {
+          description: ''
+        }
+      ],
+      additionalTasks: [
+        {
+          description: ''
+        }
+      ]
+    }
+  ]
+};
+
+const educationalBackground = {
+  title: 'Educational background',
+  schools: [
+    {
+      name: 'Hochschule für Technik & Wirtschaft',
+      location: 'Berlin',
+      dateStart: new Date(),
+      dateEnd: null
+    }
+  ]
+};
+
+const certificates = {
+  
+};
+
+const additionalInformation = {
+
+};
+
+const store = {
+  workingExperience,
+  educationalBackground,
+  certificates,
+  additionalInformation
+};
+
+export default store;
